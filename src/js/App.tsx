b14264223b6825fff4279fdb5b100f6f0906e00c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-01-22 15:35:37 +0900

    :sparkles: feat: PokeAPIのfetch処理追加

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { getAllPokemon } from './utils/pokemon';
+
 const App = () => {
+  const URL = 'https://pokeapi.co/api/v2/pokemon';
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(URL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
   return <h1>Hello World</h1>;
 };
 
