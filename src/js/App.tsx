5154107e2d9c845122e97db64346d7e753aa89c2
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-03-05 13:51:12 +0900

    :sparkles: feat: ローディング機能追加

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAllPokemon } from './utils/pokemon';
 
 const App = () => {
   const URL = 'https://pokeapi.co/api/v2/pokemon';
+
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(URL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <div>{loading ? <h1>ロード中...</h1> : <h1>ポケモンデータを取得しました。</h1>}</div>
+    </>
+  );
 };
 
 export default App;
